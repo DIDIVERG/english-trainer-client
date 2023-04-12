1ef708fd385113699d65b873e31aa091e75fe769
DIDIVERG <90328061+DIDIVERG@users.noreply.github.com>
2023-04-12 20:01:01 +0300

    Added some links to navbar and offcanvas now is 70% on small displays

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,60 +1,70 @@
-import { useState } from "react";
-import './styles/header.css'
+import React, { useState } from 'react';
+import { Navbar, Nav, NavDropdown, Offcanvas, Button } from 'react-bootstrap';
+import './styles/header.css';
+import {FaBars, FaDoorOpen, FaInfoCircle, FaTimes} from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+
 function Header() {
     const [showOffcanvas, setShowOffcanvas] = useState(false);
-    const [showBackdrop, setShowBackdrop] = useState(false);
 
-    const closeOffcanvas = () => {
-        setShowOffcanvas(false);
-        setShowBackdrop(false);
-    };
+    const handleClose = () => setShowOffcanvas(false);
+    const handleShow = () => setShowOffcanvas(true);
 
     return (
-        <nav className="navbar navbar-dark bg-dark fixed-top">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                    Offcanvas dark navbar
-                </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    onClick={() => {
-                        setShowOffcanvas(!showOffcanvas);
-                        setShowBackdrop(true);
-                    }}
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                {showBackdrop && (
-                    <div
-                        className="backdrop"
-                        onClick={() => {setShowBackdrop(false); setShowOffcanvas(!showOffcanvas)}}
-                    ></div>
-                )}
-                <div
-                    className={`offcanvas offcanvas-end text-bg-dark ${
-                        showOffcanvas || showBackdrop ? "show" : ""
-                    }`}
-                    tabIndex="-1"
-                    id="offcanvasDarkNavbar"
-                    aria-labelledby="offcanvasDarkNavbarLabel"
-                >
-                    <div className="offcanvas-header">
-                        <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">
-                            Dark offcanvas
-                        </h5>
-                        <button
-                            type="button"
-                            className="btn-close btn-close-white"
-                            onClick={closeOffcanvas}
-                            aria-label="Close"
-                        ></button>
-                    </div>
-                    <div className="offcanvas-body"></div>
-                </div>
-            </div>
-        </nav>
+        <div className="nav-wrapper d-flex flex-column">
+            <Navbar bg="dark" expand="lg" className="navbar-dark navbar-light">
+                <Navbar.Brand href="#">English Trainer</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbar-nav" className="navbar-toggler-custom">
+                    {showOffcanvas ? <FaTimes onClick={handleClose} /> : <FaBars onClick={handleShow} />}
+                </Navbar.Toggle>
+                <Navbar.Collapse id="navbar-nav">
+                    <Nav className="me-auto">
+                        <NavLink exact to="/" className="nav-link" activeClassName="active" onClick={handleClose}>
+                            Dictionary
+                        </NavLink>
+                        <NavLink to="/about" className="nav-link" activeClassName="active" onClick={handleClose}>
+                            Trainers
+                        </NavLink>
+                        <NavLink to="/contact" className="nav-link" activeClassName="active" onClick={handleClose}>
+                            Videos
+                        </NavLink>
+                    </Nav>
+                    <Nav className="ms-auto">
+                        <NavLink to="/login" className="nav-link" activeClassName="active" onClick={handleClose}>
+                            <FaInfoCircle></FaInfoCircle>
+                        </NavLink>
+                        <NavLink to="/logout" className="nav-link" activeClassName="active" onClick={handleClose}>
+                            <FaDoorOpen></FaDoorOpen>
+                        </NavLink>
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+
+            <Offcanvas show={showOffcanvas} onHide={handleClose} placement="end" className="bg-dark offcanvas offcanvas-width">
+                <Offcanvas.Header className="d-flex justify-content-between align-items-center">
+                    <span>Menu</span>
+                    <FaTimes onClick={handleClose} />
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <Nav className="flex-column">
+                        <NavLink exact to="/" className="nav-link" activeClassName="active" onClick={handleClose}>
+                            Dictionary
+                        </NavLink>
+                        <NavLink to="/about" className="nav-link" activeClassName="active" onClick={handleClose}>
+                            Trainers
+                        </NavLink>
+                        <NavLink to="/contact" className="nav-link" activeClassName="active" onClick={handleClose}>
+                            Videos
+                        </NavLink>
+                        <NavLink to="/login" className="nav-link" activeClassName="active" onClick={handleClose}>
+                            Log out
+                        </NavLink>
+
+                    </Nav>
+                </Offcanvas.Body>
+            </Offcanvas>
+        </div>
     );
 }
 
