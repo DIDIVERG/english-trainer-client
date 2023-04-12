4eb1e1786a3202dc9d5b2386ecdf08ab83cfac20
DIDIVERG <90328061+DIDIVERG@users.noreply.github.com>
2023-04-12 22:53:13 +0300

    Revert "Have added float button that adds new translation inside list"
    This reverts commit 2712b25253765e57563179f4e571c83e87d99c9e.

--- src/pages/DictionaryPage.jsx
+++ src/pages/DictionaryPage.jsx
@@ -1,31 +1,12 @@
 import React, { useState } from 'react';
-import '../App.css';
-import  TranslationList  from '../components/TranslationList'
+import '../App.css'
 
-const words = [
-    { id: 1, word: 'hank yossss', translation: 'Привет' },
-    { id: 2, word: 'Goodbye', translation: 'Пока' },
-    { id: 3, word: 'Thank you', translation: 'Спасибо' },
-    { id: 3, word: 'Thank you', translation: 'Спасибо' },
-    { id: 3, word: 'Thank you', translation: 'Спасибо' },
-    { id: 3, word: 'Thank you', translation: 'Спасибо' },
-    { id: 3, word: 'Thank you', translation: 'Спасибо' },
-    { id: 3, word: 'Thank you', translation: 'Спасибо' },
-    { id: 3, word: 'Thank you', translation: 'Спасибо' },
-    { id: 3, word: 'Thank you', translation: 'Спасибо' },
-    { id: 3, word: 'Thank you', translation: 'Спасибо' },
-    { id: 3, word: 'Thank you', translation: 'Спасибо' },
-    { id: 3, word: 'Opportunity was ont found but we can addthis ', translation: 'Спасибо' },
-    { id: 3, word: 'Thank you', translation: 'Спасибо' },
-    { id: 3, word: 'Not at all', translation: 'Спасибо' },
-];
+export const DictionaryPage = () => {
 
-const DictionaryPage = () => {
     return (
-        <div className="container">
-            <TranslationList translations={words} />
+        <div >
+        <p> DICTIONARY PAGE </p>
         </div>
-    );
-};
 
-export default DictionaryPage;
+    );
+}
