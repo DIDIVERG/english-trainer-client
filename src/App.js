2712b25253765e57563179f4e571c83e87d99c9e
DIDIVERG <90328061+DIDIVERG@users.noreply.github.com>
2023-04-12 21:48:21 +0300

    Have added float button that adds new translation inside list

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css';
-import {DictionaryPage} from './pages/DictionaryPage'
+import DictionaryPage from './pages/DictionaryPage'
 import {TrainersPage} from './pages/TrainersPage'
 import {VideosPage} from './pages/VideosPage'
 import Header from './components/Header'
@@ -14,79 +14,7 @@ function App() {
     <div className="App">
       <Router>
         <Header/>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto commodi consequuntur culpa cumque
-              debitis ex expedita facilis ipsum, iure labore laboriosam maiores perspiciatis porro quibusdam quo rem
-              repudiandae, temporibus unde velit? Ab consequuntur dolor ducimus et eveniet excepturi explicabo fugit hic
-              illo in ipsam officiis praesentium quae quas, qui saepe sequi unde veniam. Accusantium aliquid ex
-              excepturi impedit laudantium magnam nemo, praesentium recusandae veritatis vitae. Animi atque, cum eius et
-              eveniet, explicabo fugit hic id impedit labore laborum maxime natus nemo odio officiis repellendus sunt.
-              Aliquid aperiam aspernatur culpa debitis, deleniti dolore doloribus earum error ex exercitationem fugit
-              impedit ipsam laborum maxime natus nisi nostrum odio optio, perferendis possimus quis quisquam recusandae
-              sit velit vero. A alias aliquid assumenda autem beatae blanditiis consectetur consequuntur corporis cumque
-              distinctio dolorem eligendi ex excepturi fugit id impedit in, incidunt iure maiores mollitia nisi, odio
-              officiis pariatur possimus quod similique veritatis vitae. Amet assumenda distinctio doloribus esse quas
-              quia quisquam repellat tempora. Beatae incidunt iusto laboriosam odio voluptates. Autem commodi cum
-              deleniti, excepturi impedit ipsa ipsum iure libero nemo, nisi porro possimus quam rerum similique
-              temporibus? Ab accusantium adipisci alias amet asperiores autem blanditiis consequatur cumque debitis
-              distinctio dolor, esse maiores natus, necessitatibus nulla repudiandae sint soluta temporibus ullam vel.
-              A, ad dolorem illo reiciendis repellendus similique? Accusamus aliquam amet architecto facere hic labore
-              laboriosam maxime nesciunt optio voluptates. Assumenda at deleniti, doloribus, earum eveniet
-              exercitationem illo illum ipsam ipsum iste iure magni natus nihil quo reprehenderit, similique unde vitae
-              voluptatem? Adipisci culpa, eos illum minima nostrum quod repellat similique sint tempore velit. Alias,
-              architecto at aut beatae dicta dolor dolorem eaque eos est ipsam maiores modi mollitia nam nulla,
-              provident reiciendis sed ut. Ab ad aliquam consequatur consequuntur corporis delectus enim error incidunt
-              laboriosam nihil nisi non nostrum, odio perspiciatis quia, quis sapiente sit tenetur. Amet eos ex, harum
-              hic inventore ipsam itaque laudantium reprehenderit similique sunt totam ut vitae voluptatum! Ab, aut eos
-              error impedit modi necessitatibus odio optio placeat quod reprehenderit! Aliquid consectetur cum deleniti
-              ducimus et facilis iure libero, officiis, quae quasi quisquam sunt ullam vel? Adipisci aperiam asperiores
-              consequuntur debitis deserunt dignissimos dolor dolorem eos excepturi, laudantium necessitatibus quaerat,
-              rem repellendus rerum sapiente sed ullam. Accusantium aliquam, asperiores assumenda consequuntur,
-              cupiditate debitis dolorum ea earum eligendi est facere fugit harum inventore ipsa, iusto libero magni
-              molestias mollitia necessitatibus numquam odio odit pariatur placeat provident quaerat quas quisquam quo
-              quos repudiandae tempore totam unde velit voluptates? Commodi dolores dolorum excepturi ipsam ipsum,
-              molestias placeat ullam? Adipisci aliquam aperiam asperiores cum dolorem doloribus earum illum minus
-              molestias nam necessitatibus odio quod quos rem repellendus, sed, veritatis vero. Asperiores at
-              consequatur culpa cupiditate dicta distinctio, doloremque doloribus, est facilis in magni odit officia,
-              qui rem soluta temporibus veritatis. Commodi cumque esse, excepturi explicabo hic incidunt ipsum magni
-              minus nulla officiis perspiciatis rem reprehenderit, rerum sequi temporibus! Aliquam cupiditate doloremque
-              eius error ipsa praesentium reprehenderit, rerum. Asperiores assumenda atque expedita maxime nemo optio
-              provident, recusandae repudiandae ut. Ad aliquid autem expedita itaque laudantium magni minima molestias
-              quia quod, similique. Animi, iure minima nam perspiciatis qui recusandae vitae! Adipisci aliquid at autem
-              corporis, cumque distinctio fuga fugiat fugit illo incidunt iste laboriosam laborum maxime minima neque
-              non officiis omnis quae quas quasi, quo ratione repudiandae voluptas! Amet beatae cupiditate delectus
-              deserunt eligendi, est id illum ipsum libero magnam minima natus numquam quae reprehenderit sit, tempore
-              voluptatibus. Aperiam beatae commodi, consequatur corporis cum cumque debitis delectus distinctio ea esse
-              et ex facere facilis illo impedit itaque labore laborum minus mollitia natus nemo nesciunt numquam
-              officiis omnis pariatur perferendis porro quaerat quidem ratione reiciendis reprehenderit repudiandae,
-              similique soluta tenetur ullam voluptate voluptatem. Cumque eaque laboriosam quae. Facere ipsam minima
-              saepe? Beatae cumque, ex libero quasi qui tempora voluptatibus! Asperiores nihil non placeat sed
-              voluptatibus! Ab aliquid at atque culpa cumque debitis delectus, dicta doloremque ducimus enim eveniet
-              exercitationem fugiat ipsum laudantium modi molestias nam necessitatibus nemo neque nobis nostrum numquam
-              optio quam quasi quia quidem rem repellat unde vel, velit! Alias, commodi delectus doloribus earum
-              exercitationem illo in inventore necessitatibus obcaecati omnis perferendis porro quas, quia quisquam quod
-              ratione recusandae sed tempora tenetur, voluptate. Aliquam cumque cupiditate debitis dignissimos impedit
-              ipsa ipsum magnam, maiores minus omnis pariatur provident quisquam tempora unde veniam veritatis vero
-              voluptates. Accusamus ad aut beatae corporis culpa debitis dolores, esse eum eveniet, exercitationem
-              explicabo ipsam iste magni perferendis quam quia quo soluta suscipit temporibus unde vel veniam veritatis.
-              Beatae consectetur fuga, fugiat obcaecati officiis omnis perferendis quod? Autem cumque ducimus ipsum
-              minus natus quae velit. Animi, aspernatur commodi culpa cum distinctio dolorum facere fugit illum ipsa
-              modi necessitatibus neque odit optio quam quas qui repellendus reprehenderit, repudiandae sit veniam? Ad
-              animi autem, ducimus ea enim eum facere neque possimus quo repellat similique sint, unde voluptate. Ad
-              amet at autem blanditiis consequatur cum earum ex fugiat harum id illo illum incidunt inventore ipsam iste
-              libero nisi obcaecati omnis possimus provident quibusdam quidem, quisquam quod sed sit soluta tempore
-              temporibus totam unde voluptate? Accusantium debitis fugit hic nam quisquam quo? Ab aliquam aut,
-              blanditiis id inventore maxime minus modi, nesciunt quos, rerum tenetur voluptatem. Aut blanditiis in
-              ipsam sapiente velit. Alias atque aut consequuntur dignissimos dolorem doloremque ducimus ea eaque, earum
-              eius error esse est, eum eveniet exercitationem expedita facere hic illo illum inventore, ipsam itaque
-              magni mollitia natus nemo nihil nostrum numquam odio placeat provident quo quos ratione sequi similique
-              tempore ullam unde! Adipisci aliquid aut blanditiis debitis dicta dolorum earum eius, est, facere fuga
-              minima molestiae molestias mollitia nesciunt, nisi obcaecati quae quod quos rem repudiandae temporibus
-              ullam velit. A aspernatur blanditiis culpa cum deserunt dolores, eligendi, facere labore maxime nesciunt
-              nobis possimus provident quod sit ullam veniam vero! Animi aut consequuntur corporis cum debitis dolores
-              eius excepturi exercitationem facilis iusto laboriosam laudantium maxime nemo neque quaerat quidem,
-              reprehenderit sit tempora tenetur totam, vitae voluptas voluptate! Consequatur corporis cumque distinctio
-              earum eum exercitationem hic ipsum maiores, maxime natus nostrum pariatur possimus praesentium quia quidem
-              quis rem voluptas voluptates. Delectus harum, vel?</p>
+
           <Routes>
       <Route path="/main"> </Route>
       <Route path="/dictionary" element={<DictionaryPage/>}> </Route>
---
+++ src/components/TranslationList.jsx
@@ -0,0 +1,172 @@
+import { useState } from 'react';
+import { ListGroup, Button, Modal, Form } from 'react-bootstrap';
+import { useMediaQuery } from 'react-responsive';
+import './styles/transationlist.css';
+import {FaDoorOpen, FaPlus} from "react-icons/fa";
+
+function TranslationListItem({ word, translation, onDelete, onEdit }) {
+    const [showModal, setShowModal] = useState(false);
+    const [editedWord, setEditedWord] = useState(word);
+    const [editedTranslation, setEditedTranslation] = useState(translation);
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+        setEditedWord(word);
+        setEditedTranslation(translation);
+    };
+
+    const handleSaveChanges = () => {
+        onEdit(editedWord, editedTranslation);
+        handleCloseModal();
+    };
+
+    const isSmallScreen = useMediaQuery({ query: '(max-width: 576px)' });
+
+    return (
+        <>
+            <ListGroup.Item className={`d-flex justify-content-between align-items-center flex-column flex-md-row ${isSmallScreen ? 'text-center' : ''}`}>
+                <div style={{ minWidth: '50%' }} className={isSmallScreen ? 'mb-2 mb-md-0' : ''}>
+                    <h5 className="mb-0">{word}</h5>
+                    <p className="text-muted mb-0">{translation}</p>
+                </div>
+                <div>
+                    <Button variant="outline-danger" className="mr-2 delete-btn" onClick={onDelete}>
+                        Удалить
+                    </Button>
+                    <Button variant="outline-primary" className="edit-btn" onClick={() => setShowModal(true)}>
+                        Редактировать
+                    </Button>
+                </div>
+            </ListGroup.Item>
+
+            <Modal show={showModal} onHide={handleCloseModal} style={{ marginTop: '20vh' }}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Редактирование перевода</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="formWord">
+                            <Form.Label>Слово</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Введите слово"
+                                value={editedWord}
+                                onChange={(e) => setEditedWord(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group controlId="formTranslation">
+                            <Form.Label>Перевод</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Введите перевод"
+                                value={editedTranslation}
+                                onChange={(e) => setEditedTranslation(e.target.value)}
+                            />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseModal}>
+                        Отмена
+                    </Button>
+                    <Button variant="primary" onClick={handleSaveChanges}>
+                        Сохранить изменения
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+
+
+function TranslationList({ translations }) {
+    const [list, setList] = useState(translations);
+    const [showAddModal, setShowAddModal] = useState(false);
+    const [newWord, setNewWord] = useState('');
+    const [newTranslation, setNewTranslation] = useState('');
+
+    const handleDeleteTranslation = (index) => {
+        const newList = [...list];
+        newList.splice(index, 1);
+        setList(newList);
+    };
+
+    const handleEditTranslation = (index, newWord, newTranslation) => {
+        const newList = [...list];
+        newList[index] = { word: newWord, translation: newTranslation };
+        setList(newList);
+    };
+
+    const handleAddTranslation = () => {
+        const newTranslationObj = { word: newWord, translation: newTranslation };
+        setList([...list, newTranslationObj]);
+        setNewWord('');
+        setNewTranslation('');
+        setShowAddModal(false);
+    };
+
+    return (
+        <div>
+            <ListGroup className="list-group">
+                {list.map((translation, index) => (
+                    <TranslationListItem
+                        key={index}
+                        word={translation.word}
+                        translation={translation.translation}
+                        onDelete={() => handleDeleteTranslation(index)}
+                        onEdit={(newWord, newTranslation) =>
+                            handleEditTranslation(index, newWord, newTranslation)
+                        }
+                    />
+                ))}
+            </ListGroup>
+
+            <div className="fixed-add-button">
+                <Button variant="success" onClick={() => setShowAddModal(true)}>
+                    <FaPlus/>
+                </Button>
+            </div>
+
+            <Modal style={{ marginTop: '20vh' }} show={showAddModal} onHide={() => setShowAddModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Добавление нового перевода</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="formWord">
+                            <Form.Label>Слово</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Введите слово"
+                                value={newWord}
+                                onChange={(e) => setNewWord(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group controlId="formTranslation">
+                            <Form.Label>Перевод</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Введите перевод"
+                                value={newTranslation}
+                                onChange={(e) => setNewTranslation(e.target.value)}
+                            />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setShowAddModal(false)}>
+                        Отмена
+                    </Button>
+                    <Button variant="primary" onClick={handleAddTranslation}>
+                        Добавить перевод
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
+}
+
+export default TranslationList;
